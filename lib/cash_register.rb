13a7d3509d4fd107d35851040ad64853354a96a6
Irene Scott <irenejinscott@gmail.com>
2020-05-17 20:13:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,9 +14,8 @@ class CashRegister
       self.total += price * optional_amount
       optional_amount.times do 
   	@items << title
-  	
-    end
-    
+  	end
+    total -= new_transaction
     end
     
     
